a38ca477ea85977d0280fba19f6837b760abf677
Jochen Theodorou <blackdrag@gmx.org>
2013-01-15 16:24:58 +0100

    MOP2: cleaned up meta property version

--- src/main/groovy/mop/MetaProperty.java
+++ src/main/groovy/mop/MetaProperty.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2007 the original author or authors.
+ * Copyright 2003-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,63 +15,18 @@
  */
 package groovy.mop;
 
-import java.lang.reflect.Modifier;
-
-import org.codehaus.groovy.runtime.MetaClassHelper;
+import java.util.List;
 
 /**
  * Represents a property on a bean which may have a getter and/or a setter
  * 
  * @author <a href="mailto:james@coredevelopers.net">James Strachan</a>
- * @version $Revision$
+ * @author Jochen Theodorou
  */
-public abstract class MetaProperty {
-
-    protected final String name;
-    protected Class type;
-    public static final String PROPERTY_SET_PREFIX = "set";
-
-    public MetaProperty(String name, Class type) {
-        this.name = name;
-        this.type = type;
-    }
-
-    /**
-     * @return the property of the given object
-     * @throws Exception if the property could not be evaluated
-     */
-    public abstract Object getProperty(Object object);
-
-    /**
-     * Sets the property on the given object to the new value
-     * 
-     * @param object on which to set the property
-     * @param newValue the new value of the property
-     * @throws RuntimeException if the property could not be set
-     */
-    public abstract void setProperty(Object object, Object newValue);
-
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * @return the type of the property
-     */
-    public Class getType() {
-        return type;
-    }
-    
-    public int getModifiers() {
-        return Modifier.PUBLIC;
-    }
-
-    public static String getGetterName(String propertyName, Class type) {
-        String prefix = type == boolean.class || type == Boolean.class ? "is" : "get";
-        return prefix + MetaClassHelper.capitalize(propertyName);
-    }
-
-    public static String getSetterName(String propertyName) {
-        return PROPERTY_SET_PREFIX + MetaClassHelper.capitalize(propertyName);
-    }
+public interface MetaProperty {
+    public List<MetaMethod> getter(boolean accessField);
+    public List<MetaMethod> setter(boolean accessField);
+    public String getName();
+    public Class getType();
+    public int getModifiers();
 }
---
+++ src/main/groovy/mop/internal/DefaultMetaProperty.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2003-20013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package groovy.mop.internal;
+
+import groovy.mop.MetaMethod;
+import groovy.mop.MetaProperty;
+
+import java.lang.reflect.Modifier;
+import java.util.List;
+
+/**
+ * Represents a property on a bean which may have a getter and/or a setter
+ * 
+ * @author <a href="mailto:james@coredevelopers.net">James Strachan</a>
+ * @author Jochen Theodorou
+ */
+public class DefaultMetaProperty implements MetaProperty {
+    private final String name;
+    private final Class type;
+    private final List<MetaMethod> getter;
+    private final List<MetaMethod> setter;
+
+    public DefaultMetaProperty(String name, Class type, List<MetaMethod> getter, List<MetaMethod> setter) {
+        this.name = name;
+        this.type = type;
+        this.getter = getter;
+        this.setter = setter;
+    }
+
+    /**
+     * @return name of the property
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the type of the property
+     */
+    public Class getType() {
+        return type;
+    }
+
+    /**
+     * @return modifiers
+     */
+    public int getModifiers() {
+        return Modifier.PUBLIC;
+    }
+
+    @Override
+    public List<MetaMethod> getter(boolean accessField) {
+        return getter;
+    }
+
+    @Override
+    public List<MetaMethod> setter(boolean accessField) {
+        return setter;
+    }
+}
